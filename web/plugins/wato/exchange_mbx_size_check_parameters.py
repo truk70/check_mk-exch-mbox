29702ef39c654873539ed2e6cb654fc5f6e12a6b
Marius Pana <mp@sphs.ro>
2015-01-11 09:34:35 +0200

    completed switch to dictionary based params

--- web/plugins/wato/exchange_mbx_size_check_parameters.py
+++ web/plugins/wato/exchange_mbx_size_check_parameters.py
@@ -2,26 +2,48 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 # 2014 Marius Pana mp@sphs.ro
 
+#register_check_parameters(
+#    subgroup_applications,
+#    "exchange_user_mbx_size",
+#    _("MS Exchange User Mailbox Size"),
+#    Dictionary(
+#        elements = [
+#            ("Maximum Mailbox Size",
+#                Integer(
+#                    title = _("Warning if above"),
+#                    unit = _("MB"),
+#                    default_value = 1500
+#                ),
+#                Integer(
+#                    title = _("Critical if above"),
+#                    unit = _("MB"),
+#                    default_value = 2000
+#                ),
+#            ),
+#	]
+#    ),
+#    None,
+#    "dict"
+#)
+
+# -- another attempt -- #
+
 register_check_parameters(
     subgroup_applications,
     "exchange_user_mbx_size",
     _("MS Exchange User Mailbox Size"),
     Dictionary(
         elements = [
-            ("Maximum Mailbox Size",
-                Integer(
-                    title = _("Warning if above"),
-                    unit = _("MB"),
-                    default_value = 1500
-                ),
-                Integer(
-                    title = _("Critical if above"),
-                    unit = _("MB"),
-                    default_value = 2000
-                ),
-            ),
-	]
+        ("levels",
+          Tuple(
+            title = _("Maximum Exchange Mailbox Size"),
+            elements = [
+                Integer(title = _("Warning if larger than"), default_value = 1500),
+                Integer(title = _("Critical if larger than"), default_value = 2000)
+            ]
+          )
+        )]
     ),
     None,
-    "dict"
+    None
 )
