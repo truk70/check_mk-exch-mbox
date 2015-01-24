e4683ea1c54e15238fa15887364aed2f94082bdc
Marius Pana <mp@sphs.ro>
2015-01-24 14:52:38 +0200

    updated check_params for factory settings - dictionary based

--- web/plugins/wato/exchange_mbx_size_check_parameters.py
+++ web/plugins/wato/exchange_mbx_size_check_parameters.py
@@ -2,48 +2,32 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 # 2014 Marius Pana mp@sphs.ro
 
-#register_check_parameters(
-#    subgroup_applications,
-#    "exchange_user_mbx_size",
-#    _("MS Exchange User Mailbox Size"),
-#    Dictionary(
-#        elements = [
-#            ("Maximum Mailbox Size",
-#                Integer(
-#                    title = _("Warning if above"),
-#                    unit = _("MB"),
-#                    default_value = 1500
-#                ),
-#                Integer(
-#                    title = _("Critical if above"),
-#                    unit = _("MB"),
-#                    default_value = 2000
-#                ),
-#            ),
-#	]
-#    ),
-#    None,
-#    "dict"
-#)
-
-# -- another attempt -- #
-
 register_check_parameters(
     subgroup_applications,
     "exchange_user_mbx_size",
     _("MS Exchange User Mailbox Size"),
     Dictionary(
         elements = [
-        ("levels",
-          Tuple(
-            title = _("Maximum Exchange Mailbox Size"),
-            elements = [
-                Integer(title = _("Warning if larger than"), default_value = 1500),
-                Integer(title = _("Critical if larger than"), default_value = 2000)
-            ]
-          )
-        )]
+            ("levels", # Name of your parameters
+                Tuple(
+                    title = "Levels for mailboxes", # Specify a title for this parameters
+                    elements = [
+                        Integer(
+                            title = _("Warning if above"),
+                            unit = _("MB"),
+                            default_value = 1500
+                        ),
+                        Integer(
+                            title = _("Critical if above"),
+                            unit = _("MB"),
+                            default_value = 2000
+                        ),
+                    ]
+                )
+            ),
+        ],
+        optional_keys = None, # Always show this subgroup
     ),
-    None,
-    None
+    TextAscii( title = "Name of mailbox"),
+    "dict"
 )
